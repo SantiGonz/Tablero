d8d02d7494b2b56d04cf50d23816f2d76b60cc1d
Santiago Gonzalez <321090412@alumno.enp.unam.mx>
2022-05-04 12:03:14 -0500

    provocando conflicto

--- dynamics/index.php
+++ dynamics/index.php
@@ -19,7 +19,7 @@ for ($i = 0; $i < $size; $i++) {
     for ($j = 0; $j < $size; $j++) {
         if ($i % 2 === $j % 2) {
 <<<<<<< HEAD
-             echo "<td>+</td>";
+             echo "<td>++</td>";
 =======
             echo "<td><img src='../assets/blanco.png'></td>";
 >>>>>>> tablero
